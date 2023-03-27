f1127d3b4de6bc88853c3e0c1c608c4c8985234a
diasGustaavo <diasgustaavo@icloud.com>
2023-03-27 14:49:48 -0300

    Initial Commit

---
+++ arcSagacia/arcSagaciaApp.swift
@@ -0,0 +1,17 @@
+//
+//  arcSagaciaApp.swift
+//  arcSagacia
+//
+//  Created by Gustavo Dias on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct arcSagaciaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
